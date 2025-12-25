7b475624346498abb1fe2c4e6c0c8e29303ba1cd
ttrieu2025 <cattrieutruongminh@gmail.com>
2025-12-25 01:31:04 -0800

    Remove button

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,23 @@ import { FaLinkedin, FaInstagram, FaFacebook } from 'react-icons/fa';
 
 
 function App() {
-
-const [count, setCount] = useState(0)
-
+  
   return (
     <>
         <header className="social-links">
         <nav className="nav-bar">
-          <a href="#Trieu">Trieu </a>
-          <a href="#home">Home</a>
-          <a href="#resources">Resources</a>
-          <a href="#blogs">Blogs</a>
-          <a href="#projects">Projects</a>
+            <a href="#Trieu">Trieu </a>
+            <a href="#home">Home</a>
+            <a href="#resources">Resources</a>
+            <a href="#blogs">Blogs</a>
+            <a href="#projects">Projects</a>
         </nav>
         <div className="social-links">
 
       <a 
-          href="https://www.linkedin.com/in/mc-trieu-truong" 
-          target="_blank" 
-          rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/mc-trieu-truong" 
+            target="_blank" 
+            rel="noopener noreferrer"
           >
           <FaLinkedin size={30} color="#b5b4b4" />
       </a>
@@ -49,14 +47,7 @@ const [count, setCount] = useState(0)
       I currently study Electrical Engineering at the University of British Columbia. <br />
       I created this space to share knowledge and my personal journey, <br />
       I hope you will enjoy the content and find something helpful.</p>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-      </div>
-
+      </div>  
     </>
   )
 }
