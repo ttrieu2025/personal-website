4ba2129c191dcc798af6499acfdfbffbdf1edbbd
ttrieu2025 <cattrieutruongminh@gmail.com>
2025-12-30 17:57:21 -0800

    Default

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ function Home(){
       <p>
         My name is Trieu, but you can call me Trevor.<br />
         I study Electrical Engineering at UBC.<br />
-        This site shares my projects, resources, and journey and I'm gay.
+        This site shares my projects, resources, and journey.
       </p>
     </div>
   )
