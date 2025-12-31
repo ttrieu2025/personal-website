35f0c51bbe8a261ca7f77e6ecba8420a4ee77bba
ttrieu2025 <cattrieutruongminh@gmail.com>
2025-12-30 23:00:04 -0800

    add profile

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,23 @@
-function Home(){
+function Home() {
   return (
     <div className="page">
-      <p>
-        My name is Trieu, but you can call me Trevor.<br />
-        I am a second year Electrical Engineering student at UBC.<br />
-        Currently, I created this space to share my interest in mathematics and engineering, 
-        <br />
-        so I hope you will enjoy the content and find it helpful.
-      </p>
+      <div className="intro">
+        <div className="intro-text">
+          <p>
+            My name is Trieu, but you can call me Trevor.<br />
+            I am a second year Electrical Engineering student at UBC.<br />
+            Currently, I created this space to share my interest in mathematics and engineering,
+            <br />
+            so I hope you will enjoy the content and find it helpful.
+          </p>
+        </div>
+
+        <div className="intro-image">
+          <img src="/Feuerbach.svg" alt="Feuerbach Point" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
