a96fec5600c403d381586f6b9cd4186607e6994f
ttrieu2025 <cattrieutruongminh@gmail.com>
2025-12-30 20:45:19 -0800

    Change Introduction (2)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ function Home(){
       <p>
         My name is Trieu, but you can call me Trevor.<br />
         I study Electrical Engineering at UBC.<br />
-        I created this space to share my interest and personal journey,.<Homebr />
+        I created this space to share my interest and personal journey,.<br />
         I hope you will enjoy the content and find the information helpful.
       </p>
     </div>
